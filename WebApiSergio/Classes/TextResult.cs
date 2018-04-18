9114a278f6a82de914afe2b0fa6ef2fc4caa6183
Usuario <Usuario@DESKTOP-F2Q933M>
2018-04-18 04:41:11 -0300

    Retirar as referências não utiliadas do projeto nos arquivos 'using'

--- WebApiSergio/Classes/TextResult.cs
+++ WebApiSergio/Classes/TextResult.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Http;
 
 namespace WebApiSergio.Classes
--- WebApiSergio/Controllers/ClientesController.cs
+++ WebApiSergio/Controllers/ClientesController.cs
@@ -2,7 +2,6 @@
 using System.Collections.ObjectModel;
 using System.Net;
 using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
 using WebApiSergio.Classes;
